352fc4811664b53c2cacdfaa083118303c2ab009
DaviAieta <aieta.davi@gmail.com>
2024-04-27 22:59:04 -0300

    first commit

---
+++ api/src/controllers/chat.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+
+export class ChatController {
+  static async getChat(req: Request, res: Response) {
+    try {
+      return res.render("chat");
+    } catch (error) {
+      return res.status(400).send("Error: " + error);
+    }
+  }
+
+  static async postChat(req: Request, res: Response) {
+    try {
+      const message = req.body();
+      let response = "";
+
+      if (message.includes("ola")) {
+        response = "Ola, como posso ajudar?";
+      } else {
+        response = "Nao entendi sua mensagem";
+      }
+
+      return res.json({ message: response });
+    } catch (error) {
+      return res.status(400).send("Error: " + error);
+    }
+  }
+}
